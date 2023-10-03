b4e1a92f8d7ca3e592e6c144b9af78e89ea789d7
johnlombardi389 <lombardij38@gmail.com>
2023-10-03 16:07:54 -0400

    Fixed missing / after register path

--- recipeready/urls.py
+++ recipeready/urls.py
@@ -22,7 +22,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-    path('api/register', views.register_user, name='register_user'),
+    path('api/register/', views.register_user, name='register_user'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
