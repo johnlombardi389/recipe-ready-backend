52611d4be8f45fcaf5a42cc7c5e8ae21c17edb43
johnlombardi389 <lombardij38@gmail.com>
2023-11-11 16:20:45 -0500

    Updated allowed hosts in settings

--- recipeready/settings.py
+++ recipeready/settings.py
@@ -32,7 +32,7 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='john389.pythonanywhere.com').split(',')
 
 from datetime import timedelta
 
